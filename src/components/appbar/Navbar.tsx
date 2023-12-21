f61581be5d6a7ef0a88e015c3e4300feba5ca72c
Daffadon <daffaputranarendra9@gmail.com>
2023-12-21 21:17:10 +0700

    UI: Landing Page

--- src/components/appbar/Navbar.tsx
+++ src/components/appbar/Navbar.tsx
@@ -1,24 +1,24 @@
 import { Link } from "react-router-dom";
-import logo from "/M.png";
+import logo from "/M.svg";
 
 const Navbar = () => {
   return (
     <header className="sticky top-0 z-10 bg-transparent">
-      <div className="flex h-[7vh] w-full justify-between items-center bg-blue-800 opacity-80">
+      <div className="flex h-[7vh] w-full justify-around items-center bg-black opacity-95 gap-5">
         <Link to={"/"}>
           <img
             src={logo}
             alt=""
-            className="w-10 h-10 rounded-full ml-52 cursor-pointer"
+            className="w-10 h-10 rounded-full cursor-pointer border-2 border-yellow-500"
           />
         </Link>
-        <div className="mr-52 flex gap-5">
-          <div className="font-semibold hover:text-blue-700 cursor-pointer duration-150 opacity-100 text-white">
+        <div className="flex gap-5">
+          <p className="font-semibold hover:text-yellow-500 cursor-pointer duration-150 opacity-100 text-white">
             Login
-          </div>
-          <div className="font-semibold hover:text-blue-700 cursor-pointer duration-150 opacity-100 text-white">
+          </p>
+          <p className="font-semibold hover:text-yellow-500 cursor-pointer duration-150 opacity-100 text-white">
             Register
-          </div>
+          </p>
         </div>
       </div>
     </header>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -8,6 +8,7 @@ import film1 from "../assets/film1.png";
 import film2 from "../assets/film2.png";
 import film3 from "../assets/film3.png";
 import film4 from "../assets/film4.png";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -16,11 +17,10 @@ const Landing = () => {
         <Swiper
           modules={[A11y, Autoplay]}
           loop={true}
-          autoplay={{ delay: 3000 }}
-          // spaceBetween={50}
+          autoplay={{ delay: 2000 }}
           slidesPerView={1}
           pagination={{ clickable: true }}
-          className="max-h-[100vh] -mt-[3.3em] relative"
+          className="max-h-[100vh] -mt-[3.35em] relative w-screen"
         >
           <SwiperSlide>
             <img src={film1} alt="" className="blur-sm" />
@@ -34,12 +34,42 @@ const Landing = () => {
           <SwiperSlide>
             <img src={film4} alt="" className=" blur-sm" />
           </SwiperSlide>
-          <p className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white absolute z-10">
-            DAFFA ASDASDNJASID ASHDASDI
-          </p>
+          <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white absolute z-10 text-xl font-bold pt-32">
+            <p className="text-center px-5">
+              Welcome to <span className="text-yellow-500">Movi</span> , where
+              your movie journey begins! Discover, explore, and save your
+              favorite movies all in one place. Whether you're a cinephile or
+              just looking for the perfect film for your next movie night, we've
+              got you covered.
+            </p>
+            <div className="flex justify-center">
+              <Link to={"/login"}>
+                <p className="mt-10 bg-white w-max px-5 py-2 rounded-md text-lg text-black  hover:bg-black hover:text-yellow-500 duration-200 cursor-pointer">
+                  Get Started
+                </p>
+              </Link>
+            </div>
+            <div className="pt-16 flex justify-center gap-4">
+              {Array.from({ length: 11 }, (_, i) => {
+                if (i % 2 != 0) {
+                  return (
+                    <div
+                      key={i}
+                      className="h-5 w-5 bg-gray-300 rounded-full animate-bounce"
+                    ></div>
+                  );
+                }
+                return (
+                  <div
+                    key={i}
+                    className="h-5 w-5 bg-yellow-500 rounded-full animate-pulse"
+                  ></div>
+                );
+              })}
+            </div>
+          </div>
         </Swiper>
       </div>
-      <p>asdasd</p>
     </Layout>
   );
 };
